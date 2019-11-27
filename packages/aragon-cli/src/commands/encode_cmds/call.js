477c9f087ed2f32793e6bc8e6edeed9d70e139e3
Alejandro J. Santander <palebluedot@gmail.com>
2019-11-27 11:43:03 -0300

    Working encode call command

--- packages/aragon-cli/src/commands/encode_cmds/call.js
+++ packages/aragon-cli/src/commands/encode_cmds/call.js
@@ -1,15 +1,39 @@
-exports.command = 'call [option]'
+const fs = require('fs')
+const { ensureWeb3 } = require('../../helpers/web3-fallback')
 
-exports.describe = 'Encode a call to a contract'
+exports.command = 'call [contractArtifacts] [functionName] [functionArgs..]'
+
+exports.describe = 'Encode a call to a function of a contract'
 
 exports.builder = yargs => {
-  return yargs.positional('option', {
-    describe: 'Option description',
-  })
+  return yargs
+    .positional('contractArtifacts', {
+      describe: 'Path to the JSON contract artifacts containing its ABI',
+    })
+    .positional('functionName', {
+      describe: 'Name of the function to call',
+    })
+    .positional('functionArgs', {
+      description: 'Arguments to be passed to the function',
+      array: true,
+      default: [],
+    })
 }
 
-exports.handler = async function({ option }) {
-  console.log(`call ${option}`)
+exports.handler = async function({
+  network,
+  contractArtifacts,
+  functionName,
+  functionArgs,
+}) {
+  const web3 = await ensureWeb3(network)
+
+  const file = fs.readFileSync(contractArtifacts, 'utf8')
+  const artifacts = JSON.parse(file)
+
+  const Contract = new web3.eth.Contract(artifacts.abi)
+  const call = Contract.methods[functionName](...functionArgs).encodeABI()
+  console.log(call)
 
   process.exit(0)
 }
